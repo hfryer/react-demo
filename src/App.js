32b5e91a89cdcdde385b36a97e87ae65f9f14c5f
Harry Fryer <5618131+hfryer@users.noreply.github.com>
2020-12-02 00:32:27 +0000

    Here is my react stub routing test.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import {HashRouter, Link, Route} from "react-router-dom";
+import {Link, Switch, Route} from "react-router-dom";
 
 function App() {
     return (
-        <HashRouter basename='/'>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Here is my example react website stub.
-                    </p>
-                    <ul>
-                        <li><Link to={'/about'}>About</Link></li>
-                    </ul>
-                </header>
-                <Route path="/about" component={About}/>
-            </div>
-        </HashRouter>
+        <Switch>
+            <Route path='/about'>
+                <About/>
+            </Route>
+            <Route path='/projects'>
+                <Projects/>
+            </Route>
+            <Route path='/'>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p>
+                            Here is my example react website stub.
+                        </p>
+                        <ul>
+                            <li><Link to='/about'>About</Link></li>
+                            <li><Link to='/projects'>Projects</Link></li>
+                        </ul>
+                    </header>
+                </div>
+            </Route>
+        </Switch>
     );
 }
 
 const About = () => <div><h2>About</h2></div>
+const Projects = () => <div><h2>Projects</h2></div>
 
 export default App;
