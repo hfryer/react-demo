4c494ef386e25c005d6bc6a6d1b7ac9cf45eafbe
Harry Fryer <harryfryer@me.com>
2020-12-14 23:19:04 +0000

    Adding in basic bank holiday code.

--- src/componants/home/bank-holidays/BankHolidays.tsx
+++ src/componants/home/bank-holidays/BankHolidays.tsx
@@ -1,40 +1,67 @@
 import React from "react";
 import {Card} from "react-bootstrap";
+import axios from 'axios';
+import {DateTime} from 'luxon'
 
 interface BankHolidayItem {
-    date: string
+    title: string
+    date: DateTime
 }
 
 interface BankHolidaysState {
     bankHolidays: Array<BankHolidayItem>
 }
+interface BankHolidayItemProps{
+    item: BankHolidayItem
+}
 
-export const RenderBankHolidayItem: React.FC<BankHolidayItem> = (item) => {
-    return (<li>{item.date}</li>);
+export const RenderBankHolidayItem: React.FC<BankHolidayItemProps> = ({item}) => {
+    let convertedDate = item.date.toLocaleString(DateTime.DATE_FULL);
+    return (<li>{item.title} - {convertedDate}</li>);
 }
 
 class BankHolidays extends React.Component<any, BankHolidaysState> {
+
+    private now = DateTime.local();
+
     constructor(props: any) {
         super(props);
         this.state = {
-            bankHolidays: [
-                {date: '2020-10-11'},
-                {date: '2020-10-11'},
-                {date: '2020-10-11'},
-                {date: '2020-10-11'}
-            ]
+            bankHolidays: []
         }
+    }
+
+    componentDidMount() {
+        axios.get('https://www.gov.uk/bank-holidays.json').then(
+            res => {
+                const data = res.data
+                let events = data['england-and-wales'].events
+
+                const convertedEvents: Array<BankHolidayItem> = events.map((item: any, index: any) => {
+                    return {
+                        title: item.title,
+                        date: DateTime.fromISO(item.date, {locale: 'gb'})
+                    }
+                })
+
+                const filteredEvents: Array<BankHolidayItem> = convertedEvents.filter((item: any) => {
+                        return item.date > this.now;
+                    }
+                )
+                this.setState({bankHolidays: filteredEvents})
+            }
+        )
     }
 
     render() {
         return <Card className='m-3'>
             <Card.Header>
-                <h4>Bank Holidays</h4>
+                <h4>English Bank Holidays</h4>
             </Card.Header>
             <Card.Body>
                 <ul>
                     {this.state.bankHolidays.map((item, index) => {
-                        return <RenderBankHolidayItem {...item}/>
+                        return <RenderBankHolidayItem key={index} item={item}/>
                     })}
                 </ul>
             </Card.Body>
